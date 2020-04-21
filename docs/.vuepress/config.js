e8d42dc60440c0d8849e1c68b22dca936c01a83c
chenyulin <cyl@addcn.com>
2020-04-21 11:33:37 +0800

    feat(all): 創建倉庫

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    base: '/learn-ts/',
+    title: 'ts學習',
+    description: 'ts學習記錄',
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/xxxxxxx/blog-demo',
+        // 自定义仓库链接文字。
+        repoLabel: 'github',
+        nav: [
+            { text: '首页', link: '/' },
+            { text: 'first', link: '/ts/first.md' }
+        ],
+        sidebar: [
+            ['/', '首页'],
+            ['/ts/first.md', '我的第一篇博客']
+        ]
+    }
+  }
